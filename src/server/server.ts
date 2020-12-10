3dd1605fbf09c116604fbf2f645bb8d6a905c74d
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-12-09 21:05:53 -0600

    Fixed the routing issue with a global error handler on the server. Manually going to any route or reloading triggers actions OUTSIDE of my Links will directly attempt to fetch that url from the server, bypassing my API and causing an error. The server handler will now catch that and send the homepage as a response, preserving the location of the former page/route

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,8 @@ import apiRouter from './routes';
 import * as morgan from 'morgan';   // Using this import syntax due to issues with typings for the exports
 import * as helmet from 'helmet';   // Seen in issues like this here https://github.com/iamkun/dayjs/issues/475
 import * as cors from 'cors';       // Same for cors
+import * as path from 'path';
+
 const app = express();
 
 app.use(express.static('public'));
@@ -13,6 +15,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', apiRouter);
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
